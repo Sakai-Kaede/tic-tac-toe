de7794aa82656604a43e9aaf755726f103c2a258
Sakai-Kaede <kana1024k@gmail.com>
2024-05-04 20:48:39 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <>
-
-    </>
-  )
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
 }
 
-export default App
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={() => {}} />
+        <Square value={squares[2]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => {}} />
+        <Square value={squares[4]} onSquareClick={() => {}} />
+        <Square value={squares[5]} onSquareClick={() => {}} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => {}} />
+        <Square value={squares[7]} onSquareClick={() => {}} />
+        <Square value={squares[8]} onSquareClick={() => {}} />
+      </div>
+    </>
+  );
+}
+
+export default Board;
